86958e02016638eaaa368d8eff84c2996a18eeaa
ShubhraJain <00sankalp@gmail.com>
2018-01-04 20:06:44 -0800

    Create todo input validation done

--- react-client/src/components/CreateTodo.jsx
+++ react-client/src/components/CreateTodo.jsx
@@ -2,11 +2,55 @@ import React from 'react';
 
 class CreateTodo extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null
+    }
+  }
+
+  renderError() {
+    if (!this.state.error) {
+      return null
+    }
+
+    return (
+      <div style={{color: 'red'}}>
+        {this.state.error}
+      </div>
+    );
+  }
+
   handleAdd(event) {
     event.preventDefault();
     var newTodo = this.refs.newTask.value;
+
+    const validateInput = this.validateInput(newTodo);
+
+    if (validateInput) {
+      this.setState({
+        error: validateInput
+      });
+      return;
+    }
+
+    this.setState({
+      error: null
+    });
+
     this.props.createTask(newTodo);
     this.refs.newTask.value = '';
+  }
+
+  validateInput(task) {
+    if (!task) {
+      return 'Please enter a task';
+    } else if(_.find(this.props.todos, todo => todo.task === task)) {
+      return 'Task already exist';
+    } else {
+      return null;
+    }
   }
 
   render() {
@@ -14,6 +58,7 @@ class CreateTodo extends React.Component {
       <form onSubmit={this.handleAdd.bind(this)}>
         <input type="text" placeholder="new todo..." ref="newTask" />
         <button>Add</button>
+        {this.renderError()}
       </form>
     )
   }
